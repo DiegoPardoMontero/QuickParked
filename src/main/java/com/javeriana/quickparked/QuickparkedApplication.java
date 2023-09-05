53ebc3871506d9f04580b37aae2949db81f9d102
Diego Pardo Montero <di-pardo@javeriana.edu.co>
2023-09-05 02:06:41 -0500

    Creada estructura inicial con Spring Boot

---
+++ src/main/java/com/javeriana/quickparked/QuickparkedApplication.java
@@ -0,0 +1,13 @@
+package com.javeriana.quickparked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickparkedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickparkedApplication.class, args);
+	}
+
+}
